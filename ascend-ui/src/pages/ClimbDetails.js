c1e8d4e2b25cf5015d48ea6c75bc798005a14044
jcanz <3466642+jcanzonetta@users.noreply.github.com>
2022-03-16 23:49:14 -0700

    Add tooltip for safety ratings and remove tag references

--- ascend-ui/src/pages/ClimbDetails.js
+++ ascend-ui/src/pages/ClimbDetails.js
@@ -5,7 +5,7 @@ import ClimbPhotos from "../components/ClimbPhotos";
 import Map from "../components/Map";
 import ClimbResult from "../components/ClimbResult";
 import { IoMdPhotos } from "react-icons/io";
-import { MdDescription, MdLocationPin, MdTag } from "react-icons/md";
+import { MdDescription, MdLocationPin } from "react-icons/md";
 import { HomeHeader } from "../components/HomeHeader";
 
 function ClimbDetails() {
@@ -35,7 +35,14 @@ function ClimbDetails() {
         <h2>{climb.route_name}</h2>
         <div className="difficulty-header">
           <h3>{"grade" in climb ? climb.grade.YDS : <p>Ungraded</p>}</h3>
-          <h4>{climb.safety}</h4>
+          <h4
+            className="tooltip"
+            tooltip-data={
+              'G: Ample protection.\nPG: Generally good protection with a few sections of poor protection.\nPG-13: Fair protection that may result in a long fall.\nR: "Runnout." Limited Protection and potentital for seriouis injury.\nX: Very limited or no protection with ground fall potential.'
+            }
+          >
+            {climb.safety}
+          </h4>
         </div>
         <div>
           <h5>
@@ -53,15 +60,8 @@ function ClimbDetails() {
           <div>
             {climb.protection && climb.protection[0].lnegth > 0
               ? climb.protection
-              : "No additional protection infoformation."}
+              : "No additional protection information."}
           </div>
-        </div>
-        <div>
-          <h5>
-            <MdTag />
-            Tags:
-          </h5>
-          <div>variable number of tags shown here</div>
         </div>
         <div>
           <h5>
--- ascend-ui/src/pages/HomePage.js
+++ ascend-ui/src/pages/HomePage.js
@@ -4,23 +4,10 @@ import Weather from "../components/Weather";
 import { HomeHeader } from "../components/HomeHeader";
 
 function HomePage() {
-  const [filterHelperShown, setFilterHelperShown] = useState(false);
-
   return (
     <>
       <HomeHeader />
       <ClimbSearch placeholder="Search for a climb, location, or quality."></ClimbSearch>
-      <div
-        className="home-filters-container"
-        onMouseEnter={() => setFilterHelperShown(true)}
-        onMouseLeave={() => setFilterHelperShown(false)}
-      >
-        Additional Filters:{" "}
-        {filterHelperShown && (
-          <div>Apply a filter to narrow down your search!</div>
-        )}
-      </div>
-      <div className="home-filters">[Common filters will show up here.]</div>
       <Weather className="weather-component"></Weather>
     </>
   );
